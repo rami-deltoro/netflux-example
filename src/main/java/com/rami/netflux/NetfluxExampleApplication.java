cd6a85d4af38c9e8b050d78b8fadd1fe2a15a785
rami stefanidis <rami.stefanidis@gmail.com>
2019-01-21 15:01:34 -0500

    Initial Commit

---
+++ src/main/java/com/rami/netflux/NetfluxExampleApplication.java
@@ -0,0 +1,14 @@
+package com.rami.netflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetfluxExampleApplication.class, args);
+    }
+
+}
+
